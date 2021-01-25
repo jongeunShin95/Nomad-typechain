7949aacbe52fb0c7ab2f4bade4fcd517b20ea3d8
jongeunShin95 <jongeunshin95@gmail.com>
2021-01-25 20:55:05 +0900

    Update 7/#0.7 Classes on Typescript part One (05:05)

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,8 +1,21 @@
-interface Human {
-    name: String;
-    age: Number;
-    gender: String;
+class Human {
+    public name: String;
+    public age: Number;
+    public gender: String;
+    constructor(name: String, age: Number, gender: String) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
+
+const lynn = new Human("Lynn", 18, "female");
+
+// interface Human {
+//     name: String;
+//     age: Number;
+//     gender: String;
+// }
 
 const person = {
     age: 22,
@@ -15,6 +28,6 @@ const sayHi = (person: Human): String  => {
 };
 
 // console.log(sayHi("Nicolas", 444, "male"));
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
